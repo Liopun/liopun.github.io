ba9205904fd783ef9ce9f11167e4e2e0025aad10
Captone Habiyaremye <hcaptone@gmail.com>
2019-11-16 06:29:42 -0500

    more functionalities

--- src/components/ProfileIntroSection/index.js
+++ src/components/ProfileIntroSection/index.js
@@ -10,21 +10,21 @@ const ProfileIntroSection = ({ content }) =>
       <Content>{content()}</Content>
     </InfoWrapper>
     <StyledImage
-      src="/images/profile/michele-mazzucco-portrait.jpg"
-      alt="Michele Mazzucco - Interdisciplinary Designer"
+      src="../../images/profile/michele-mazzucco-portrait.jpg"
+      alt="Captone Habiyaremye - Front-end Developer"
       loader={({ isLoaded }) => <Loader isLoaded={isLoaded} />}
       sources={[{
         media: 'max-width: 40rem',
         srcset: [
-          '/images/profile/michele-mazzucco-portrait-square.jpg 1x',
-          '/images/profile/michele-mazzucco-portrait-square-2x.jpg 2x',
-          '/images/profile/michele-mazzucco-portrait-square-3x.jpg 3x'
+          '../../images/profile/michele-mazzucco-portrait-square.jpg 1x',
+          '../../images/profile/michele-mazzucco-portrait-square-2x.jpg 2x',
+          '../../images/profile/michele-mazzucco-portrait-square-3x.jpg 3x'
         ]
       },{
         srcset: [
-          '/images/profile/michele-mazzucco-portrait.jpg 1x',
-          '/images/profile/michele-mazzucco-portrait-2x.jpg 2x',
-          '/images/profile/michele-mazzucco-portrait-3x.jpg 3x'
+          '../../images/profile/michele-mazzucco-portrait.jpg 1x',
+          '../../images/profile/michele-mazzucco-portrait-2x.jpg 2x',
+          '../../images/profile/michele-mazzucco-portrait-3x.jpg 3x'
         ]
       }]}
     />
