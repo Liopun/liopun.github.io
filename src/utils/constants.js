04097a24a0762c5c5ff342d599bafc1b06464953
Captone Habiyaremye <hcaptone@gmail.com>
2020-09-21 06:15:44 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,11 +5,11 @@ export const META = {
     image: '/images/captone-meta-image.png',
   },
   index: {
-    title: 'Captone Habiyaremye | Full stack developer',
+    title: 'Captone Habiyaremye | Full-stack Developer',
     description: 'Captone Habiyaremye, Full stack developer with 6 years in the industry. software development has been more than a job for me, it has provided an engaging challenge to keep learning and improving my skills in delivering exceptional, high-quality software. As I\'m looking forward to push-forward my career as a full stack developer, I\'m very familiar and experienced with web application architecture and have worked with a good number of successful dev teams.'
   },
   profile: {
     title: 'Captone Habiyaremye | Profile',
-    description: 'Full stack developer with 6 years in the industry.'
+    description: 'Full-stack Developer with 6 years in the industry.'
   }
 }
